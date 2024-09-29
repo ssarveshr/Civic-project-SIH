55754b7948a0d17162bda32e9279a06e6de7fd1b
Sai Sarvesh R <ssr1032005@gmail.com>
2024-09-29 10:59:54 +0530

    added seperate js file

---
+++ script.js
@@ -0,0 +1,12 @@
+var rectangle2 = document.getElementById("rectangle2");
+if(rectangle2) {
+    rectangle2.addEventListener("click", function (e) {
+        window.location.href='https://www.youtube.com'
+      	});
+	}
+var creacc=document.getElementById("creacc");
+    if(creacc){
+	creacc.addEventListener("click",function(e){
+		window.location.href='https://www.rnsit.ac.in'
+		});
+	}
